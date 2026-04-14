753b51d4ecf84f39a4418637a52b666d03aa1fbf
Will-Massey <william@fortisaccounts.com>
2026-04-14 17:44:37 +0100

    fix: restore Express namespace augmentation with eslint-disable comment

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -5,9 +5,12 @@ import { JwtPayload, UserRole } from '../types';
 import { logger } from '../utils/logger';
 
 // Extend Express Request type
-declare module 'express' {
-  interface Request {
-    user?: JwtPayload;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
+    }
   }
 }
 
